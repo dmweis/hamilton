965df7817286e9ae60c344b551846d1615860961
David Weis <davidmichaelweis@gmail.com>
2019-11-04 13:52:52 +0000

    Add new hamilton cobs stepper controller

--- arduino_firmware/stepper_hamilton/stepper_hamilton.ino
+++ arduino_firmware/stepper_hamilton/stepper_hamilton.ino
@@ -1,27 +1,83 @@
+#include <PacketSerial.h>
 #include <AccelStepper.h>
 
-AccelStepper LeftBackWheel(1, 2, 5);   // (Type:driver, STEP, DIR) - Stepper1
-AccelStepper LeftFrontWheel(1, 4, 7);  // Stepper2
-AccelStepper RightBackWheel(1, 3, 6);  // Stepper3
-AccelStepper RightFrontWheel(1, 12, 13); // Stepper4
+PacketSerial_<COBS, 0, 500> packetSerial;
 
+AccelStepper LeftBackWheel(1, 2, 5);
+AccelStepper LeftFrontWheel(1, 4, 7);
+AccelStepper RightBackWheel(1, 3, 6);
+AccelStepper RightFrontWheel(1, 12, 13);
 
-void setup() {
-  LeftFrontWheel.setMaxSpeed(3000);
-  LeftBackWheel.setMaxSpeed(3000);
-  RightFrontWheel.setMaxSpeed(3000);
-  RightBackWheel.setMaxSpeed(3000);
+void setup()
+{
+  packetSerial.begin(115200);
+  packetSerial.setPacketHandler(&onPacketReceived);
 
-  LeftFrontWheel.setSpeed(1500);
-  LeftBackWheel.setSpeed(1500);
-  RightFrontWheel.setSpeed(1500);
-  RightBackWheel.setSpeed(1500);
+  LeftFrontWheel.setMaxSpeed(300);
+  LeftBackWheel.setMaxSpeed(300);
+  RightFrontWheel.setMaxSpeed(30);
+  RightBackWheel.setMaxSpeed(300);
+
+  LeftFrontWheel.setSpeed(0);
+  LeftBackWheel.setSpeed(0);
+  RightFrontWheel.setSpeed(0);
+  RightBackWheel.setSpeed(0);
 }
-void loop() {
+void loop()
+{
+  packetSerial.update();
   // Execute the steps
   LeftFrontWheel.runSpeed();
   LeftBackWheel.runSpeed();
   RightFrontWheel.runSpeed();
   RightBackWheel.runSpeed();
-  
+}
+
+void onPacketReceived(const uint8_t *buffer, size_t size)
+{
+    if (size != 8)
+    {
+        return;
+    }
+    // Make a temporary buffer.
+    uint8_t tempBuffer[size];
+
+    // Copy the packet into our temporary buffer.
+    memcpy(tempBuffer, buffer, size);
+
+    if (tempBuffer[0])
+    {
+        LeftFrontWheel.setSpeed(tempBuffer[1]);
+    }
+    else
+    {
+        LeftFrontWheel.setSpeed(-tempBuffer[1]);
+    }
+
+    if (tempBuffer[2])
+    {
+        RightFrontWheel.setSpeed(tempBuffer[3]);
+    }
+    else
+    {
+        RightFrontWheel.setSpeed(-tempBuffer[3]);
+    }
+
+    if (tempBuffer[4])
+    {
+        LeftBackWheel.setSpeed(tempBuffer[5]);
+    }
+    else
+    {
+        LeftBackWheel.setSpeed(-tempBuffer[5]);
+    }
+
+    if (tempBuffer[6])
+    {
+        RightBackWheel.setSpeed(tempBuffer[7]);
+    }
+    else
+    {
+        RightBackWheel.setSpeed(-tempBuffer[7]);
+    }
 }
