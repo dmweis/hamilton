eeedf7baa99c9ca8daab6e950e94262f856544d9
David Weis <dweis7@gmail.com>
2021-01-02 03:46:10 +0000

    Use lss driver

--- src/driver.rs
+++ src/driver.rs
@@ -1,18 +1,6 @@
-use anyhow::Error;
 use anyhow::Result;
-use bytes::{BufMut, BytesMut};
-use futures::SinkExt;
+use lss_driver::LSSDriver;
 use std::str;
-use tokio_util::codec::{Decoder, Encoder};
-
-#[derive(thiserror::Error, Debug)]
-#[non_exhaustive]
-pub enum HamiltonError {
-    #[error("communication with motor driver failed")]
-    CommError,
-    #[error("failed opening serial port")]
-    FailedOpeningSerialPort,
-}
 
 #[derive(Default)]
 pub struct WireMoveCommand {
@@ -22,101 +10,29 @@ pub struct WireMoveCommand {
     pub wheel_d: i32,
 }
 
-impl WireMoveCommand {
-    fn encode(&self) -> Vec<u8> {
-        let mut buffer = vec![];
-        // push wheels in the weird format that I chose for some reason
-        buffer.push((self.wheel_a > 0) as u8);
-        buffer.push(self.wheel_a.abs() as u8);
-        buffer.push((self.wheel_b > 0) as u8);
-        buffer.push(self.wheel_b.abs() as u8);
-        buffer.push((self.wheel_c > 0) as u8);
-        buffer.push(self.wheel_c.abs() as u8);
-        buffer.push((self.wheel_d > 0) as u8);
-        buffer.push(self.wheel_d.abs() as u8);
-
-        let mut encoded = postcard_cobs::encode_vec(&buffer);
-        encoded.push(0);
-        encoded
-    }
-}
-
-pub struct HamiltonProtocol;
-
-impl Decoder for HamiltonProtocol {
-    type Item = ();
-    type Error = Error;
-
-    fn decode(&mut self, _: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        Ok(None)
-    }
-}
-
-impl Encoder<WireMoveCommand> for HamiltonProtocol {
-    type Error = Error;
-
-    fn encode(&mut self, data: WireMoveCommand, buf: &mut BytesMut) -> Result<(), Error> {
-        let encoded_data = data.encode();
-        buf.reserve(encoded_data.len());
-        buf.put_slice(&encoded_data);
-        Ok(())
-    }
-}
-
 pub struct HamiltonDriver {
-    framed_port: tokio_util::codec::Framed<tokio_serial::Serial, HamiltonProtocol>,
+    driver: LSSDriver,
 }
 
 impl HamiltonDriver {
     pub fn new(port: &str) -> Result<HamiltonDriver> {
-        let mut settings = tokio_serial::SerialPortSettings::default();
-        settings.baud_rate = 115200;
-        let serial_port = tokio_serial::Serial::from_path(port, &settings)
-            .map_err(|_| HamiltonError::FailedOpeningSerialPort)?;
-        Ok(HamiltonDriver {
-            framed_port: HamiltonProtocol.framed(serial_port),
-        })
+        let driver = LSSDriver::new(port)?;
+        Ok(HamiltonDriver { driver })
     }
 
     pub async fn send(&mut self, command: WireMoveCommand) -> Result<()> {
-        self.framed_port
-            .send(command)
-            .await
-            .map_err(|_| HamiltonError::CommError)?;
+        self.driver
+            .set_rotation_speed(1, command.wheel_a as f32)
+            .await?;
+        self.driver
+            .set_rotation_speed(2, command.wheel_b as f32)
+            .await?;
+        self.driver
+            .set_rotation_speed(3, command.wheel_c as f32)
+            .await?;
+        self.driver
+            .set_rotation_speed(4, command.wheel_d as f32)
+            .await?;
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn encoding_adds_trailing_zero() {
-        let move_command = WireMoveCommand::default();
-        let encoded = move_command.encode();
-        assert_eq!(*encoded.last().unwrap(), 0_u8);
-    }
-
-    #[test]
-    fn left_front_positive() {
-        let mut move_command = WireMoveCommand::default();
-        move_command.wheel_a = 255;
-        let encoded = move_command.encode();
-        let mut iter = encoded.iter();
-        assert_eq!(*iter.next().unwrap(), 3_u8);
-        assert_eq!(*iter.next().unwrap(), 1_u8);
-        assert_eq!(*iter.next().unwrap(), 255_u8);
-    }
-
-    #[test]
-    fn left_front_negative() {
-        let mut move_command = WireMoveCommand::default();
-        move_command.wheel_a = -255;
-        let encoded = move_command.encode();
-        let mut iter = encoded.iter();
-        assert_eq!(*iter.next().unwrap(), 1_u8);
-        assert_eq!(*iter.next().unwrap(), 2_u8);
-        assert_eq!(*iter.next().unwrap(), 255_u8);
     }
 }
