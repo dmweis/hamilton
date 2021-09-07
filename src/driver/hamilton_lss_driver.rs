2bd383918615a3b1a3c017b2b54c887ddcb5c538
David Weis <dweis7@gmail.com>
2021-09-07 22:41:35 +0100

    Add dc motor driver

--- src/driver/hamilton_lss_driver.rs
+++ src/driver/hamilton_lss_driver.rs
@@ -1,12 +1,9 @@
-use crate::holonomic_controller::HolonomicWheelCommand;
-
-use super::{HamiltonDriver, MotorCommand};
+use super::{BodyConfig, Clampable, HamiltonDriver, MotorCommand};
+use crate::{driver::MotorConfig, holonomic_controller::HolonomicWheelCommand};
 use anyhow::Result;
 use async_trait::async_trait;
-use directories::ProjectDirs;
 use lss_driver::{LSSDriver, LedColor};
-use serde::{Deserialize, Serialize};
-use std::{fs::create_dir_all, path::Path, str, sync::Arc};
+use std::sync::Arc;
 use tokio::sync::Mutex;
 
 pub struct HamiltonLssDriver {
@@ -58,101 +55,27 @@ impl HamiltonDriver for HamiltonLssDriver {
     }
 }
 
-trait Clampable {
-    fn clamp_num(&self, a: Self, b: Self) -> Self;
+#[derive(Debug)]
+pub struct LssWireMoveCommand {
+    motors: [MotorCommand; 4],
 }
 
-impl Clampable for f32 {
-    fn clamp_num(&self, a: Self, b: Self) -> Self {
-        let min = a.min(b);
-        let max = a.max(b);
-        if *self > max {
-            max
-        } else if *self < min {
-            min
-        } else {
-            *self
-        }
+impl LssWireMoveCommand {
+    pub fn new(motors: [MotorCommand; 4]) -> Self {
+        Self { motors }
+    }
+
+    pub fn motors(&self) -> &[MotorCommand; 4] {
+        &self.motors
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct MotorConfig {
-    id: u8,
-    inverted: bool,
+trait ConfigMappable {
+    fn apply_commands_by_mapping(&self, command: &HolonomicWheelCommand) -> LssWireMoveCommand;
 }
 
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct BodyConfig {
-    pub left_front_controller: MotorConfig,
-    pub right_front_controller: MotorConfig,
-    pub left_rear_controller: MotorConfig,
-    pub right_rear_controller: MotorConfig,
-    pub multiplier: f32,
-}
-
-impl BodyConfig {
-    pub fn load(path: &str) -> Result<Self> {
-        let file = std::fs::File::open(path)?;
-        let reader = std::io::BufReader::new(file);
-        let parsed = serde_json::from_reader(reader)?;
-        Ok(parsed)
-    }
-
-    pub fn save(&self, path: &str) -> Result<()> {
-        let file = std::fs::File::create(path)?;
-        let writer = std::io::BufWriter::new(file);
-        serde_json::to_writer_pretty(writer, self)?;
-        Ok(())
-    }
-
-    /// Try to load config from default system location
-    ///
-    /// If not present will write default config there
-    pub fn load_from_default() -> Result<Self> {
-        // TODO: This functionality may not belong here but whatever
-        if let Some(project_dirs) = ProjectDirs::from("com", "David Weis", "Hamilton controller") {
-            let config_dir = project_dirs.config_dir();
-            create_dir_all(config_dir)?;
-            let config_path = Path::new(config_dir).join("wheel_config.json");
-            let config_path = config_path.to_str().unwrap();
-            if let Ok(config) = Self::load(config_path) {
-                Ok(config)
-            } else {
-                let default = Self::default();
-                default.save_to_default()?;
-                Ok(default)
-            }
-        } else {
-            panic!("Failed to find system specific config folder\nIs $HOME not defined?");
-        }
-    }
-
-    /// Save to default location
-    pub fn save_to_default(&self) -> Result<()> {
-        // TODO: This method duplicated a lot from load_from_default
-        if let Some(project_dirs) = ProjectDirs::from("com", "David Weis", "Hamilton controller") {
-            let config_dir = project_dirs.config_dir();
-            create_dir_all(config_dir)?;
-            let config_path = Path::new(config_dir).join("wheel_config.json");
-            let config_path = config_path.to_str().unwrap();
-            Self::default().save(config_path)?;
-            Ok(())
-        } else {
-            panic!("Failed to find system specific config folder\nIs $HOME not defined?");
-        }
-    }
-
-    pub fn get_ids(&self) -> [u8; 4] {
-        [
-            self.left_front_controller.id,
-            self.right_front_controller.id,
-            self.left_rear_controller.id,
-            self.right_rear_controller.id,
-        ]
-    }
-
-    pub fn apply_commands_by_mapping(&self, command: &HolonomicWheelCommand) -> WireMoveCommand {
+impl ConfigMappable for BodyConfig {
+    fn apply_commands_by_mapping(&self, command: &HolonomicWheelCommand) -> LssWireMoveCommand {
         fn create_motor_data(mapping: &MotorConfig, value: f32) -> MotorCommand {
             if mapping.inverted {
                 MotorCommand::new(mapping.id, value * -1.0)
@@ -177,22 +100,7 @@ impl BodyConfig {
             &self.right_rear_controller,
             (command.right_rear() * self.multiplier).clamp_num(-clamp_range, clamp_range),
         );
-        WireMoveCommand::new([left_front, right_front, left_rear, right_rear])
-    }
-}
-
-#[derive(Debug)]
-pub struct WireMoveCommand {
-    motors: [MotorCommand; 4],
-}
-
-impl WireMoveCommand {
-    pub fn new(motors: [MotorCommand; 4]) -> Self {
-        Self { motors }
-    }
-
-    pub fn motors(&self) -> &[MotorCommand; 4] {
-        &self.motors
+        LssWireMoveCommand::new([left_front, right_front, left_rear, right_rear])
     }
 }
 
