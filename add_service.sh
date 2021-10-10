48606564fb5a067bcab937cdee3e9869031e49b4
David Weis <dweis7@gmail.com>
2021-10-10 16:06:21 +0100

    Update add_service script to use pi user

--- add_service.sh
+++ add_service.sh
@@ -2,10 +2,11 @@ cat <<EOT | sudo tee /etc/systemd/system/hamilton.service > /dev/null
 [Unit]
 Description=hamilton
 [Service]
+User=$(whoami)
 Type=simple
 Restart=on-failure
 RestartSec=5s
-ExecStart=/home/$(whoami)/.cargo/bin/hamilton_controller --body_config /home/pi/.config/hamiltoncontroller/wheel_config.json
+ExecStart=/home/$(whoami)/.cargo/bin/hamilton_controller --body_config /home/$(whoami)/.config/hamiltoncontroller/wheel_config.json
 [Install]
 WantedBy=default.target
 EOT
